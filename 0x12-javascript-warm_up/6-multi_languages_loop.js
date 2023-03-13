8b3417287166c8111ff64e349b3d601f1f31b3a4
chineduCoded <chinedujohn17@gmail.com>
2023-03-13 15:17:21 -0700

    Loop to languages

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/*
+   A JS Script that prints the elements of an array in a single line.
+
+   Using for loop to get this done, however other loop can still work.
+
+*/
+const lang = ['c is fun', 'Python is cool', 'JavaSript is amazing'];
+for (const item of lang) {
+  console.log(item);
+}
