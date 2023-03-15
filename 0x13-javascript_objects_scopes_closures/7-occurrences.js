fc52ba37ce95c5bfe82cf1f882d2ea499fe6efb9
chineduCoded <chinedujohn17@gmail.com>
2023-03-14 20:04:50 -0700

    Edited

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
-  let nOccurrences = 0;
+  let count = 0;
   for (let i = 0; i < list.length; i++) {
     if (searchElement === list[i]) {
-      nOccurrences++;
+      count++;
     }
   }
-  return nOccurrences;
+  return count;
 };
