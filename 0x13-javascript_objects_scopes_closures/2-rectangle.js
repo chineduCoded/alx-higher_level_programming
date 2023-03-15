9feb9ae19ddc4cb993b544f3b0c937ae6bbc4d1a
chineduCoded <chinedujohn17@gmail.com>
2023-03-14 18:41:49 -0700

    Fixed error

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor(w, h) {
-    if (w <= 0 || h <= 0) {
-      // If w or h is not positive, create an empty object
-      return {};
-    } else {
-      // Initialize the instance attributes width and height
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
       this.width = w;
       this.height = h;
     }
