28088776e477a53a7bdce05b6c4330cf86dfeeda
chineduCoded <chinedujohn17@gmail.com>
2023-03-14 20:37:20 -0700

    Error fixed

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,12 +1,7 @@
 #!/usr/bin/node
+
 exports.converter = function (base) {
-  function convert(number, base) {
-    if (number < base) {
-      return number.toString();
-    }
-    let remainder = number % base;
-    let quotient = Math.floor(number / base);
-    return convert(quotient, base) + remainder.toString();
-  }
-  return convert;
-}
+  return function (num) {
+    return num.toString(base);
+  };
+};
