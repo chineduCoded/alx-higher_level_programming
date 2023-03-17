c08f648a7f18ba7baa0cbfa4eea56628dbd22d0d
chineduCoded <chinedujohn17@gmail.com>
2023-03-17 16:17:15 -0700

    Error fix

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
 import sys
+
 
 if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
