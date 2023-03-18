963c7274445954d8df81e66c766af148c1b011aa
chineduCoded <chinedujohn17@gmail.com>
2023-03-17 19:29:31 -0700

    Fixing error

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,19 +1,35 @@
 #!/usr/bin/python3
-""" prints the State object with the name passed as argument from the database
+"""
+Script that lists all City objects from the database hbtn_0e_14_usa
 """
 import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 from model_city import City
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    # Set up connection to the database
+    db_username = sys.argv[1]
+    db_password = sys.argv[2]
+    db_name = sys.argv[3]
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+                           .format(db_username, db_password, db_name),
+                           pool_pre_ping=True)
+
+    # Create all tables in the database
     Base.metadata.create_all(engine)
+
+    # Create a session to communicate with the database
     Session = sessionmaker(bind=engine)
     session = Session()
-    for instance in (session.query(State.name, City.id, City.name)
-                     .filter(State.id == City.state_id)):
-        print(instance[0] + ": (" + str(instance[1]) + ") " + instance[2]) 
+
+    # Query the database to retrieve all City objects and print them out
+    for state, city in session.query(State, City)\
+            .filter(State.id == City.state_id)\
+            .order_by(City.id).all():
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
+
+    # Close the session
+    session.close()
