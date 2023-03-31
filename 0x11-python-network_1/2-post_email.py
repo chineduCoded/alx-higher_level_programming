984ea53bcdfbfa81da5dea9ebd9ff22e41079236
chineduCoded <chinedujohn172gmail.com>
2023-03-31 15:40:47 -0700

    sends a POST request to the passed URL with the email

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Post an Email
+"""
+import urllib.parse
+import urllib.request
+import sys
+
+
+def send_post_request(url, email):
+    """
+    Sends POST request to an url
+    Params:
+        url(str): the url to send POST request
+        email(str): to receive POST request
+    Returns: Decoded email
+    """
+    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+    with urllib.request.urlopen(url, data) as res:
+        output = res.read().decode("utf-8")
+        print(output)
+
+
+if __name__ == "__main__":
+    # Get the URL and email from command line arguments
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    print("Your email is: " + email)
+
+    send_post_request(url, email)
