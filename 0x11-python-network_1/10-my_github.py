fb7dae563c2a8c5dd190c88a7332af9a6545a0e3
chineduCoded <chinedujohn172gmail.com>
2023-03-31 18:44:09 -0700

    Fixed error

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
+"""
+Access Github and use user info
+"""
 import requests
+from requests.auth import HTTPBasicAuth
 import sys
 
 if __name__ == '__main__':
     username = sys.argv[1]
     password = sys.argv[2]
     url = 'https://api.github.com/user'
+    auth = HTTPBasicAuth(username, password)
 
-    res = requests.get(url, auth=(username, password))
+    res = requests.get(url, auth=auth)
     data = res.json()
 
     if 'id' in data:
         print(data['id'])
     else:
-        print("Could not retrieve user ID")
+        print("None")
