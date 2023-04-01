8ed0144f228f35d7ae7f829e6b77b7a50aad7f97
chineduCoded <chinedujohn172gmail.com>
2023-03-31 18:06:58 -0700

    Refactored code

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,25 +7,12 @@ import urllib.request
 import sys
 
 
-def send_post_request(url, email):
-    """
-    Sends POST request to an url
-    Params:
-        url(str): the url to send POST request
-        email(str): to receive POST request
-    Returns: Decoded email
-    """
-    data = urllib.parse.urlencode({'email': email}).encode('utf8')
-    with urllib.request.urlopen(url, data) as res:
-        output = res.read().decode("utf-8")
-        print(output)
-
-
 if __name__ == "__main__":
     # Get the URL and email from command line arguments
     url = sys.argv[1]
     email = sys.argv[2]
-
-    print(f"Your email is: {email}")
-
-    send_post_request(url, email)
+    values = {"email": email}
+    data = urllib.parse.urlencode(values).encode("utf-8")
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as r:
+        print(r.read().decode("utf-8"))
