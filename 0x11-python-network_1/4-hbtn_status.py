c170864c5a8feb579f13756bc626ca80ae243ad8
chineduCoded <chinedujohn172gmail.com>
2023-03-31 17:07:41 -0700

    Using request module to fetch data

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Fetches https://alx-intranet.hbtn.io/status
+"""
+import requests
+
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    res = requests.get(url)
+
+    print("Body response:")
+    print("\t- type: {}".format(type(res.text)))
+    print("\t- content: {}".format(res.text))
