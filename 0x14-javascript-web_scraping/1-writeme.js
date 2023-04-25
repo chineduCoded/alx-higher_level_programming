2b63043e7f8581aad68f1b442a4b55ae827f8cea
chineduCoded <chinedujohn172gmail.com>
2023-04-25 02:15:22 -0700

    task o and task 1

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+const filePath = process.argv[2];
+const stringToWrite = process.argv[3];
+
+fs.writeFile(filePath, stringToWrite, 'utf-8', (err) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(`${filePath} written successfully!`);
+  }
+});
