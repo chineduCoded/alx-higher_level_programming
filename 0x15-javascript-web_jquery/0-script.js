2aea6f98b51a070c59ebbe97f92918d744d0f9fc
chineduCoded <chinedujohn172gmail.com>
2023-05-03 13:27:22 -0700

    Some task added

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,5 +1,5 @@
 // Select the <header> element using document.querySelector
-var headerElement = document.querySelector('header');
+const headerElement = document.querySelector('header');
 
 // Update the text color of the <header> element to red (#FF0000)
 headerElement.style.color = '#FF0000';
