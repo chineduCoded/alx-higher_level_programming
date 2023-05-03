8c8c64e51d554254baf755e3718920d9368f8798
chineduCoded <chinedujohn172gmail.com>
2023-05-03 14:46:25 -0700

    Togglle classes

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $('#toggle_header').click(function () {
+    $('header').toggleClass('red green');
+  });
+});
+$(document).ready(function () {
+  $('#toggle_header').click(function () {
+    $('header').toggleClass('red green');
+  });
+});
